33e18a4c1ee5e5fcd9755f4df760254d90d20f95
Jamie Williams <136656673+EyeInDisguise@users.noreply.github.com>
2026-08-22 00:09:28 +1000

    updated input settings and initalised some stuff for PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Stuff")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 16f;
+    
+    // Components
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    
+    // Input System
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        controls = new PlayerControls();
+    }
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
     }
 }
