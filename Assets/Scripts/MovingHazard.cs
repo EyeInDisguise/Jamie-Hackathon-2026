a2c2a9db55b19f6af9d4d8ba2cf8819258e6d54d
Jamie Williams <136656673+EyeInDisguise@users.noreply.github.com>
2026-08-22 15:03:18 +1000

    Moving hazard now resumes where it was stopped

--- Assets/Scripts/MovingHazard.cs
+++ Assets/Scripts/MovingHazard.cs
@@ -7,6 +7,7 @@ public class MovingHazard : MonoBehaviour
     [SerializeField] private float moveDistance = 5f;
         
     private Vector3 startPosition;
+    private float movementTime;
 
     private void Start()
     {
@@ -18,8 +19,11 @@ public class MovingHazard : MonoBehaviour
         // Freeze the object's movement while time stop is active
         if (player.IsTimeStopped) return;
         
+        // Will resume exactly where it stopped rather than in front
+        movementTime += Time.deltaTime;
+        
         // Pingpong just makes the value go up and down between 0 and moveDistance
-        float movement = Mathf.PingPong(Time.time * moveSpeed, moveDistance);
+        float movement = Mathf.PingPong(movementTime * moveSpeed, moveDistance);
         // offset from orig position instead of always adding movement
         transform.position = startPosition + Vector3.up * movement;
     }
